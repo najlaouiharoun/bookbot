7c7df2f2a365ce5bf86f263f2e1b4832c81b1ce2
najlaouiharoun <doodadyxd@gmail.com>
2025-10-26 00:13:50 +0100

    letter count

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from stats import word_count
-
+from stats import repetition_count
 def main():
-
+    letter_count = repetition_count(get_book_text())
     word_num = word_count(get_book_text())
     print(f"Found {word_num} total words")
+    print(f"and this is the lettter count: {letter_count}")
 
 
 
--- stats.py
+++ stats.py
@@ -1,4 +1,16 @@
-def word_count(content):
-    content_list = content.split()
-    return len(content_list)
+def word_count(book_strig):
+    word_list = book_strig.split()
+    return len(word_list)
+
+def repetition_count(book_strig):
+    lowercase_book_string = book_strig.lower()
+    letter_dictionary = {}
+    for letter in lowercase_book_string:
+        if letter in letter_dictionary:
+            letter_dictionary[letter]+=1
+        else:
+            letter_dictionary[letter]=1
+    return letter_dictionary
+
+
 
